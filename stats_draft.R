885a8b7de9de14d8fc4912ce91927366080feb27
RossCoppin <coppinross@gmail.com>
2018-09-06 11:41:40 +0200

    house cleaning

--- stats_draft.R
+++ stats_draft.R
@@ -1,49 +1,42 @@
-library(tidyverse)
-library(RColorBrewer)
-library(gridExtra)
-library(lubridate)
-library(GGally)
-library(ggpubr)
-library(ggrepel)
+#library(tidyverse)
+#library(RColorBrewer)
+#library(gridExtra)
+#library(lubridate)
+#library(GGally)
+#library(ggrepel)
 
-kelp_ar <- read.csv(file = "data/kelp_ar.csv", check.names = FALSE, sep = ";")
-kelp_lgth <- read.csv(file ="data/kelp_length.csv", check.names = FALSE, sep = ";")
-data("varespec")
+#kelp_ar <- read.csv(file = "data/kelp_ar.csv", check.names = FALSE, sep = ";")
+#kelp_lgth <- read.csv(file ="data/kelp_length.csv", check.names = FALSE, sep = ";")
+#data("varespec")
 
 ## community
+# setup
 
+library(ggpubr)
 library(vegan)
 library(MASS)
-library(reshape2)
-
+library(tidyverse)
 
 flora_df <- read.csv(file = "data/flora.csv", sep = ";", check.names = FALSE)
 fauna_df <- read.csv(file = "data/fauna.csv", check.names = FALSE, sep = ";")
 benthic <- merge(flora_df,fauna_df,by=c("site","plot", "id"))
-cover_df <- read.csv(file = "data/cover.csv", check.names = FALSE, sep = ";")
-benthic_pa <- vegdist(benthic_v2, binary = TRUE)
 
-summary(flora_df)
-summary(fauna_df)
+ben_bord <- benthic %>% 
+  filter(site == "Bordjies")
+ben_oud <- benthic %>% 
+  filter(site == "Oudekraal")
+ben_bet <- benthic %>% 
+  filter(site == "Betty's Bay")
 
-benthic_v2 <- subset(benthic, select = c(4:77))
-benthic.dis <- vegdist(benthic_v2)  
-benthic.mds0 <- isoMDS(benthic.dis)
-stressplot(benthic.dis,benthic.mds0)
-benthic.mds <- metaMDS(benthic_v2, trace = FALSE)
-benthic.mds
-plot(benthic.mds, type = "t")
+# Subset site names
+bord_site <- subset(benbord, select = c(1))
 
-#RDA
-
-# standardise wave measurements
-
-benthic_v2 <- subset(benthic, select = c(4:77))
-benthic_v3 <- benthic_v2 %>%
+bord_mat <- subset(ben_bord, select = c(4:77))
+bord_mat_std <- bord_mat %>%
   decostand(method = "standardize")
 
 # standardise measurements
-cover_v2 <- subset(cover_df, select = c(4:9))
+cover_mat <- subset(cover_df, select = c(4:9))
 cover_v3 <- cover_v2 %>%
   decostand(method = "standardize")
 
